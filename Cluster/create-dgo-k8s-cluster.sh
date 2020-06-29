2633f0275e55d971c587d79eb6e2c1c0b32eb4cf
amartyamandal <amartya.mandal@gmail.com>
2020-06-29 09:24:40 -0400

    merged changes

--- Cluster/create-dgo-k8s-cluster.sh
+++ Cluster/create-dgo-k8s-cluster.sh
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 doctl k8s cluster create dgo-cluster-05 --region tor1 --version 1.18.3-do.0 --tag demo --size s-1vcpu-2gb --count 3
 doctl kubernetes cluster kubeconfig save dgo-cluster-05
 kubectl config use-context do-tor1-dgo-cluster-05
-=======
-doctl k8s cluster create dgo-cluster-04 --region tor1 --version 1.18.3-do.0 --tag demo --size s-1vcpu-2gb --count 3
-doctl kubernetes cluster kubeconfig save dgo-cluster-04
-kubectl config use-context do-tor1-dgo-cluster-04
->>>>>>> 8bfc859f96ded253b4a0a84fb052193f7c891cdc
