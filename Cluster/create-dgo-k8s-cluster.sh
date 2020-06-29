7f9cbb57fba4569b9e794de6cc87b1cf9d7fb11b
Amartya Mandal <amartya.mandal@gmail.com>
2020-06-28 23:16:47 -0400

    k8s version changed to the latest

--- Cluster/create-dgo-k8s-cluster.sh
+++ Cluster/create-dgo-k8s-cluster.sh
@@ -1,2 +1,2 @@
-doctl k8s cluster create dgo-cluster-04 --region tor1 --version 1.17.5-do.0 --tag demo --size s-1vcpu-2gb --count 3
+doctl k8s cluster create dgo-cluster-04 --region tor1 --version 1.18.3-do.0 --tag demo --size s-1vcpu-2gb --count 3
 doctl kubernetes cluster kubeconfig save dgo-cluster-04
