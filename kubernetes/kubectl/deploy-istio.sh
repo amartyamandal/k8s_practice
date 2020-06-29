0063d3c486a43f87536243f4e066c57cef3215e2
Amartya Mandal <amartya.mandal@gmail.com>
2020-06-28 23:53:10 -0400

    creates ip txt
    gg

--- kubernetes/kubectl/deploy-istio.sh
+++ kubernetes/kubectl/deploy-istio.sh
@@ -5,3 +5,5 @@ export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -
 export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
 export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
 export TCP_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="tcp")].port}')
+
+echo $INGRESS_HOST > lb-ip.txt
