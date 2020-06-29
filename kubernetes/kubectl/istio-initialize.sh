384fa35280a3d913a014c8c063eb667dc58e881e
Amartya Mandal <amartya.mandal@gmail.com>
2020-06-29 11:41:43 -0400

    ss
    ss

--- kubernetes/kubectl/istio-initialize.sh
+++ kubernetes/kubectl/istio-initialize.sh
@@ -1,4 +1,4 @@
-kubectl label namespace default istio-injection=enabled
+kubectl label --overwrite namespace default istio-injection=enabled
 
 export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
@@ -8,4 +8,4 @@ export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressga
 
 export TCP_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="tcp")].port}')
 
-echo $INGRESS_HOST > ~/lb-ip.txt
+echo $INGRESS_HOST
