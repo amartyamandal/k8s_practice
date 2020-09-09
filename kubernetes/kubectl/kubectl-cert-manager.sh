91f516d27c62e21d629f8952bb56c3a9d1bd6080
Amartya Mandal <amartya.mandal@gmail.com>
2020-09-09 16:48:23 -0400

    Update kubectl-cert-manager.sh

--- kubernetes/kubectl/kubectl-cert-manager.sh
+++ kubernetes/kubectl/kubectl-cert-manager.sh
@@ -1,2 +1,2 @@
 kubectl create namespace cert-manager
-kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.15.1/cert-manager.yaml
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.16.1/cert-manager.yaml
